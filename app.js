19ae22eef8a4aa3e423371714821e9ad70dfc576
Sanchay Javeria <javeria2@illinois.edu>
2017-04-16 18:40:03 -0500

    index.htm rendered

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var express = require('express'),
-    app     = express();
+var express    = require('express'),
+    bodyparser = require("body-parser"),
+    app        = express();
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+// app.set('view engine', 'htm');
+app.engine('htm', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+	res.render('index.htm');
+});
 
 app.listen(process.env.PORT || 8080, function(){
 	console.log('server started on port ' + 8080);
