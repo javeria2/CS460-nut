b8bb85ce1db7a1f53e5ae57f3c0e2ddc6544c8bd
Ju Young Kim <juyoungkim@wirelessprv-10-192-196-186.near.illinois.edu>
2017-04-23 21:08:05 -0500

    asdasd

--- app.js
+++ app.js
@@ -18,9 +18,26 @@ app.get('/chat', function(req, res){
 
 io.sockets.on('connection', function(socket){
 	socket.on('send message', function(data){
-		io.sockets.emit('new message', data);
+		io.sockets.emit('new message', encrypt_msg(data));
+		console.log(data);
 	});
 });
+
+/**
+Shuffler
+http://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
+**/
+function encrypt_msg(msg){
+	var shuffledWord = '';
+	var charIndex = 0;
+	msg = msg.split('');
+	while (msg.length > 0) {
+		charIndex = msg.length * Math.random() << 0;
+		shuffledWord += msg[charIndex];
+		msg.splice(charIndex, 1);
+	}
+	return shuffledWord;
+}
 
 server.listen(8080, function(){
 	console.log('server started on port ' + 8080);
---
+++ cred.js
@@ -0,0 +1,7 @@
+exports.accountSid = function() {
+    return 'AC34479a70c954123a2cb3b8ad7db79b57';
+};
+
+exports.authToken = function() {
+    return '18b312b63cc79ba1ff22f73e5477bfd0';
+};
