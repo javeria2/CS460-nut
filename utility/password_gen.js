7b6f2daaa4429be3e7a2ce9ed370105bec525809
daejung83 <daejung1@gmail.com>
2017-04-23 20:38:10 -0500

    blah

--- utility/password_gen.js
+++ utility/password_gen.js
@@ -2,7 +2,9 @@ var generator = require('generate-password');
 
 var password = generator.generate({
     length: 20,
-    numbers: true
+    numbers: true,
+    symbols: true,
+    strict: true
 });
 
 console.log(password);
