b8240811e92d133af9f614c3c211f9a231336173
Sanchay Javeria <javeria2@illinois.edu>
2017-04-23 20:45:41 -0500

    major refactors

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+//dependencies
 var express    = require('express'),
     bodyparser = require("body-parser"),
     app        = express(),
     server     = require('http').createServer(app),
-    io         = require('socket.io').listen(server);
+    io         = require('socket.io').listen(server),
+    init       = require("./routes/init"),
+    chat       = require("./routes/chat");
 
+//config
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.engine('htm', require('ejs').renderFile);
 
-app.get('/', function(req, res){
-	res.render('index.htm');
-});
+// ==============
+// ROUTES BEGIN
+// ==============
 
-app.get('/chat', function(req, res){
-	res.render('chat.htm');
-});
+// homepage, intial route
+app.use(init);
+app.use('/chat', chat);
 
 io.sockets.on('connection', function(socket){
 	socket.on('send message', function(data){
@@ -22,6 +26,11 @@ io.sockets.on('connection', function(socket){
 	});
 });
 
+// ================
+// START THE SERVER
+// ================
 server.listen(8080, function(){
 	console.log('server started on port ' + 8080);
 });
+
+
