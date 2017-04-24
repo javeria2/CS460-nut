0a849973a1015b9090346db265a0de431620ffed
arahoomam <arahoomam@gmail.com>
2017-04-23 20:34:19 -0500

    integrated 2factor auth

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ var express    = require('express'),
     bodyparser = require("body-parser"),
     app        = express(),
     server     = require('http').createServer(app),
-    io         = require('socket.io').listen(server);
+    io         = require('socket.io').listen(server),
+    twilio     = require('twilio'),
+    speakeasy  = require('speakeasy'),
+    credential = require('./cred'),
+    twoFactorAuth = require('./twoFactorAuth.js');
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.engine('htm', require('ejs').renderFile);
 
 app.get('/', function(req, res){
+    var secret  = twoFactorAuth.startAuth(credential.accountSid(),
+                         credential.authToken(),
+                         twilio,
+                         speakeasy);
+    // twoFactorAuth.authenticateToken(speakeasy, tokenAndSecret[0], tokenAndSecret[1]);
 	res.render('index.htm');
 });
 
